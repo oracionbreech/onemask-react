bd87eda5dd70d83f8bcbbea66d20269538b46575
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2021-11-10 20:44:11 +0800

    chore: not listed and price

---
+++ src/assets/remoteImages.ts
@@ -0,0 +1,2 @@
+export const currencyIcon =
+  "https://global-uploads.webflow.com/613533d8304dbb6484e4b03d/614b31cad6c93c2397734612_avalanche-avax-logo.png";
--- src/components/NFT.tsx
+++ src/components/NFT.tsx
@@ -8,14 +8,54 @@ import {
 } from "@mui/material";
 import { get } from "lodash";
 import React from "react";
+import { currencyIcon } from "../assets/remoteImages";
 import { useMetadata } from "../hooks/metadata";
 import Attributes from "./Attributes";
 
 const NFT: React.FC<{ id?: number }> = ({ id }) => {
   const { nft, loading } = useMetadata(Number(id));
 
+  const topBid = (
+    <Typography
+      textAlign='start'
+      gutterBottom
+      variant='subtitle1'
+      component='div'
+    >
+      Top Bid <img src={currencyIcon} alt='currency-icon' width={15} />{" "}
+      {Math.floor(Math.random() * 24).toFixed(2)}
+    </Typography>
+  );
+
+  const notListed = (
+    <Typography
+      textAlign='start'
+      gutterBottom
+      variant='subtitle1'
+      component='div'
+    >
+      Not Listed
+    </Typography>
+  );
+
+  const price = (
+    <Typography
+      textAlign='start'
+      gutterBottom
+      variant='subtitle1'
+      component='div'
+    >
+      Price <img src={currencyIcon} alt='currency-icon' width={15} />{" "}
+      {Math.floor(Math.random() * 1000).toFixed(2)}
+    </Typography>
+  );
+
   const NFTContent = (
-    <Card>
+    <Card
+      sx={{
+        minHeight: 560,
+      }}
+    >
       <CardMedia
         sx={{
           minHeight: 233,
@@ -48,6 +88,18 @@ const NFT: React.FC<{ id?: number }> = ({ id }) => {
           </Typography>
         </Stack>
         <Attributes />
+        <Stack direction='row' justifyContent='space-between' mt={2}>
+          {Number(id) % 2 === 0 ? price : notListed}
+          <Stack direction='row' justifyContent='space-between'>
+            <Typography gutterBottom variant='subtitle1' component='div'>
+              Last <img src={currencyIcon} alt='currency-icon' width={15} />{" "}
+              {Math.random().toFixed(2)}
+            </Typography>
+          </Stack>
+        </Stack>
+        <Stack direction='row' justifyContent='space-between'>
+          {Number(id) % 2 === 0 && topBid}
+        </Stack>
       </CardContent>
     </Card>
   );
